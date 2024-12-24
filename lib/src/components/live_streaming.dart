f1b69acf2fde5db8c19b6bd885197272638c69ac
yuyanjun <yuyanjun@zego.im>
2024-12-24 16:27:00 +0800

    toast can close by config.showToast

--- lib/src/components/live_streaming.dart
+++ lib/src/components/live_streaming.dart
@@ -148,9 +148,12 @@ class _ZegoUIKitPrebuiltLiveStreamingState extends State<ZegoLiveStreamingPage>
         return context;
       });
     }
-    ZegoLiveStreamingToast.instance.init(contextQuery: () {
-      return context;
-    });
+    ZegoLiveStreamingToast.instance.init(
+      enabled: widget.config.showToast,
+      contextQuery: () {
+        return context;
+      },
+    );
 
     subscriptions
       ..add(
